e565946b56b5b474f3c069c1974a4882b6f17f57
xhluca <git@xinghanlu.com>
2025-09-06 19:14:35 -0400

    add award committee page

--- main.py
+++ main.py
@@ -266,6 +266,7 @@ def coi_policy():
 def committees():
     data = _data()
     data["committee"] = site_data["committee"]["committee"]
+    data["award_committee"] = sorted(site_data["committee"]["award_committee"], key=lambda x: x["name"])
     data["AreaChairs"] = open("AreaChairs.md").read()
     return render_template("committees.html", **data)
 
